e994e828c828c0be42ddaa903a300e20636f4972
Peter Hollman <peterjhollman@gmail.com>
2019-08-06 11:54:56 -0500

    Completed easy fix exercise

--- src/Author.java
+++ src/Author.java
@@ -3,10 +3,9 @@
  */
 import java.util.ArrayList;
 import java.util.List;
-
+@SuppressWarnings("unchecked")
 public class Author extends Person {
     private List books;
-
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
         books = new ArrayList();
@@ -16,19 +15,19 @@ public class Author extends Person {
      * @deprecated Use publishedBooks instead
      */
     @Deprecated
+    @SuppressWarnings("all")
     public List<String> getBooks() {
         return books;
     }
-
+    @SuppressWarnings("unused")
     public List<String> publishedBooks() {
         return books;
     }
-
     public void addBook(String book) {
         books.add(book);
     }
 
-    @Override
+
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
